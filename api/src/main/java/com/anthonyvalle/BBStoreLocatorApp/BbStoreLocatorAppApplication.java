1da749e2737fc2a1e2377908fc588359c43df3a9
Anthony Valle <valleganthony@gmail.com>
2020-08-07 22:20:38 -0400

    added a .gitignore

---
+++ api/src/main/java/com/anthonyvalle/BBStoreLocatorApp/BbStoreLocatorAppApplication.java
@@ -0,0 +1,13 @@
+package com.anthonyvalle.BBStoreLocatorApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbStoreLocatorAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbStoreLocatorAppApplication.class, args);
+	}
+
+}
